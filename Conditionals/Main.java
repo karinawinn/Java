8f9b9277b49602f9c05162695050eaaa1d7eab2a
karinawinn <156015334+karinawinn@users.noreply.github.com>
2025-01-25 19:48:18 -0700

    Add files via upload

--- Conditionals/Main.java
+++ Conditionals/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main() {
+        double x = 34.5;
+        double y = 59.6;
 
+        // Should print x is less than y
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+        else if (x < y) {
+            System.out.println("x is less than y");
+        }
+        else {
+            System.out.println("x is equal to y");
+        };
+    }
+}
