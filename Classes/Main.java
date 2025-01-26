2699c16b74d9b129e36777e900002e9eea52ae39
karinawinn <156015334+karinawinn@users.noreply.github.com>
2025-01-25 19:47:07 -0700

    Add files via upload

--- Classes/Main.java
+++ Classes/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main() {
+        MyClass number = new MyClass(25); // Creates new instance of MyClass
 
+        number.plusTwo();
+        number.showNumber(); // Should print 27
+        number.plusOne();
+        number.plusTwo();
+        number.showNumber(); // Should print 30
+    }
+}
