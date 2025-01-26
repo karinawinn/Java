7db468f3611b4ab6b6a31fe9a75b696b0c762665
karinawinn <156015334+karinawinn@users.noreply.github.com>
2025-01-25 19:49:14 -0700

    Add files via upload

--- Functions/Main.java
+++ Functions/Main.java
@@ -1,1 +1,11 @@
-
+public class Main {
+    // This function returns the given number times 9 plus 32 divided by 10
+    static double function(double number) {
+        return ((number*9) + 32)/10;
+    }
+    public static void main() {
+        double number = 27.5;
+        double answer = function(number); // Runs number through function
+        System.out.println(answer); // answer should be 27.95
+    }
+}
